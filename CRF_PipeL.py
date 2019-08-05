efc645ac4881e764fa782cfd43bbe1defd828412
XcLouisWang <52888953+XcLouisWang@users.noreply.github.com>
2019-08-05 12:40:29 +0800

    Add files via upload

--- CRF_PipeL.py
+++ CRF_PipeL.py
@@ -8,27 +8,42 @@ from sklearn.metrics import classification_report,accuracy_score, auc, confusion
 from sklearn_crfsuite import metrics
 from sklearn_crfsuite import CRF
 import matplotlib.pylab as plt
+from copy import deepcopy
 from sklearn_crfsuite import scorers
+from ner_evaluation.ner_eval import collect_named_entities
+from ner_evaluation.ner_eval import compute_metrics
+from ner_evaluation.ner_eval import compute_precision_recall_wrapper
+from ner_evaluation.ner_eval import Evaluator
+from collections import defaultdict
 import nltk
 import sklearn
-import preprocess
+import crf_preprocess
 import os
 import numpy as np
+nltk.download('averaged_perceptron_tagger')
+dic = open('D:\MedHelp_100d.txt','r',encoding='UTF-8').read()
+dic = dic.split('\n')
+for i in range(len(dic)):
+    dic[i] = dic[i].split(' ')
 objects = []
-for i in range(len(os.listdir('D:/cadec/text'))):
+for i in range(int(0.01*len(os.listdir('D:/cadec/text')))):
     text_filename = os.listdir('D:/cadec/text')[i]
     Ori_filename = os.listdir('D:/cadec/Ori')[i]
-    objects.append(document('D:/cadec/text/'+text_filename,'D:/cadec/Ori/'+Ori_filename)) 
+    objects.append(document('D:/cadec/text/'+text_filename,'D:/cadec/Ori/'+Ori_filename,dic))
+    print(i)
+sel = (len(objects))
 train_cut = int(0.75*(len(objects)))
 test_cut = int((len(objects)))
-x_train,y_train = transform(objects[:train_cut])
-x_test,y_test = transform(objects[train_cut:test_cut])
+x_train,y_train = transform(objects[:train_cut],dic)
+x_test,y_test = transform(objects[train_cut:test_cut],dic)
 x_train,y_train = clean(x_train,y_train)
+print(len(x_train),(x_train[0]))
+print(len(y_train),(y_train))
 x_test,y_test = clean(x_test,y_test)
 clf =CRF( 
-     algorithm='lbfgs', 
+     algorithm='lbfgs',
      c1=0.8, 
-     c2=0.1, 
+     c2=0.12, 
      all_possible_transitions=True 
  ) 
 clf.fit(x_train,y_train)
@@ -36,12 +51,52 @@ y_pred=clf.predict(x_test)
 y_pred_train=clf.predict(x_train)
 metrics.flat_f1_score(y_train, y_pred_train,average='weighted',labels=clf.classes_)
 print(metrics.flat_classification_report(y_test, y_pred, labels=clf.classes_, digits=3 )) 
+print(y_test)
+print(y_pred)
+        
 
+metrics_results = {'correct': 0, 'incorrect': 0, 'partial': 0,
+                   'missed': 0, 'spurious': 0, 'possible': 0, 'actual': 0, 'precision': 0, 'recall': 0}
+
+# overall results
+results = {'strict': deepcopy(metrics_results),
+           'ent_type': deepcopy(metrics_results),
+           'partial':deepcopy(metrics_results),
+           'exact':deepcopy(metrics_results)
+          }
+
+
+# results aggregated by entity type
+evaluation_agg_entities_type = {e: deepcopy(results) for e in ['Symptom']}
+
+for true_ents, pred_ents in zip(y_test, y_pred):
+    tmp_results, tmp_agg_results = compute_metrics(
+        collect_named_entities(true_ents), collect_named_entities(pred_ents),  ['Symptom']
+    )
+    
+    for eval_schema in results.keys():
+        for metric in metrics_results.keys():
+            results[eval_schema][metric] += tmp_results[eval_schema][metric]
+            
+    # Calculate global precision and recall
         
-        
-        
-        
-        
+    results = compute_precision_recall_wrapper(results)
+
+
+    # aggregate results by entity type
+ 
+    for e_type in ['Symptom']:
+
+        for eval_schema in tmp_agg_results[e_type]:
+
+            for metric in tmp_agg_results[e_type][eval_schema]:
+                
+                evaluation_agg_entities_type[e_type][eval_schema][metric] += tmp_agg_results[e_type][eval_schema][metric]
+                
+        # Calculate precision recall at the individual entity level
+                
+        evaluation_agg_entities_type[e_type] = compute_precision_recall_wrapper(evaluation_agg_entities_type[e_type])
+print(results)
         
         
         
--- crf_preprocess.py
+++ crf_preprocess.py
@@ -9,14 +9,16 @@ import os
 import re
 import string
 import nltk
+import numpy as np
+global past
 class document():
-    def __init__(self,text,origin):
+    def __init__(self,text,origin,dic):
         self.annotation = re.split("\n",open(origin).read())
         self.name = text
-        self.text = nltk.word_tokenize(open(text).read())
-        self.gram = nltk.pos_tag(self.text,tagset = 'universal')
+        self.text = nltk.word_tokenize(open(text).read())        
         i = 0
         annotation_copy = re.split("\n",open(origin).read())
+        self.gram = nltk.pos_tag(self.text,tagset = 'universal')
         while i < len(self.annotation):
             if self.annotation[i] == "":
                 del self.annotation[i]
@@ -35,43 +37,50 @@ class document():
         for i in range(length):
             self.tag[i] = self.annotation[i][1]
             self.span[i] = re.split(' ',''.join(c for c in annotation_copy[i] if c not in string.punctuation))
-
-def features(obj,index):
-        return [(
-               obj.text[index],
-               bytes(len(obj.text[index])),
-               '' if index == 0 else obj.text[index-1],
-               '' if index == len(obj.text)-1 else obj.text[index+1],
-              str(obj.text[index][0].upper() == obj.text[index][0]),
-              str(obj.text[index].isalpha()),
-              obj.gram[index][1],
-              '' if index == 0 else obj.gram[index-1][1],
-               '' if index == len(obj.text)-1 else obj.gram[index+1][1]
-                )]
-def tag(obj,index):
+        self.ori_text =[[] for i in range(len(self.text))]
+        for i in range(len(self.text)):
+            self.ori_text[i] = self.text[i] 
+        for g in range(len(self.text)):
+            flag = 0
+            res = [0 for i in range(100)]
+            for i in range(len(dic)):
+                if self.text[g].lower() == dic[i][0] and len(dic[i]) == 102:
+                    res = list(map(float,dic[i][1:101]))
+                    flag = 1
+                    break
+            self.text[g] = res
+def features(obj,index,dic):
+    name = [ str(i) for i in range(300)]
+    past_ = [0 for i in range(100)] if index == 0 else obj.text[index-1]
+    next_ = [0 for i in range(100)] if index == len(obj.text)-1 else obj.text[index+1]
+    voc =  np.append(np.append(obj.text[index],past_),next_)
+    caps = float(0.1*(obj.ori_text[index][0].upper() == obj.ori_text[index][0]) and (index!=0 and obj.ori_text[index-1] != '.'))
+    fin = np.append(np.append(voc,caps),float(0.1*len(obj.ori_text[index])))
+    return dict(zip(name,fin))
+def tag(obj,index,pa):
+    global past
     flag =0
     loc = 0
+    na = "tag"
     for i in range(len(obj.span)):
-        if obj.text[index] not in obj.span[i]: 
-            pass
-        else:
+        if (obj.ori_text[index] in obj.span[i]) and  ((len(obj.span[i]) ==1) or (obj.ori_text[index-1] in obj.span[i] or obj.ori_text[index+1] in obj.span[i])):
             flag =1
             loc = i
     if flag == 1:
-        return  {
-                 obj.tag[i]
-
-                }
+        if index != 0 and pa in ("B_"+obj.tag[loc],"I_"+obj.tag[loc]):
+                    past = "I_"+obj.tag[loc]
+        else:
+                    past = "B_"+obj.tag[loc]
     else:
-        return  {
-                "Other"
-                    }
-def transform(objects):
-    x,y = [],[]
+        past = "Other"
+    return past
+def transform(objects,dic):
+    x,y = [],[]    
+    global past
+    past = 0
     for i in range(len(objects)):
-        for g in range(len(objects[i].text)):
-                x.append((features(objects[i],g)))
-                y.append((tag(objects[i],g))) 
+                x.append([features(objects[i],g,dic) for g in range(len(objects[i].text))])
+                y.append([tag(objects[i],g,past) for g in range(len(objects[i].text))])
     return x,y
 def clean(x,y):
     i = 0
